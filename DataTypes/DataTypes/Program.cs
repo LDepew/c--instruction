1cdacfd3b635ec18bdf661ef7dbf853cd8709f04
Lance <Metal_Death_Music@yahoo.com>
2022-03-17 16:46:29 -0400

    Date Types

---
+++ DataTypes/DataTypes/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DataTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string phrase = "Giraffe Academy";
+            char grade = 'A';
+            int age = 30;
+            //float, double, decimal all for decimals
+            double gpa = 3.3;
+            bool male = true;
+
+            Console.WriteLine(false);
+
+            Console.ReadLine();
+        }
+    }
+}
