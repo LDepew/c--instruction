fe42829ed5a1a87b097fae629b03603bd5360ff6
Lance <Metal_Death_Music@yahoo.com>
2022-03-17 17:27:33 -0400

    Working With Strings

---
+++ WorkingWithStrings/WorkingWithStrings/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace WorkingWithStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //\n - new line break
+            //\" - add " to middle of string
+
+            string phrase = "Giraffe Academy";
+            string phrase2 = "Giraffe Academy" + " is cool";
+
+            Console.WriteLine(phrase.ToUpper());
+            Console.WriteLine(phrase.ToLower());
+            Console.WriteLine(phrase.Length);
+            Console.WriteLine(phrase.Contains("Academy"));
+            Console.WriteLine(phrase[2]);
+            Console.WriteLine(phrase.IndexOf("Academy"));
+            Console.WriteLine(phrase.IndexOf('f'));
+            Console.WriteLine(phrase.Substring(8,3));
+
+            Console.WriteLine(phrase2);
+
+            Console.ReadLine();
+        }
+    }
+}
