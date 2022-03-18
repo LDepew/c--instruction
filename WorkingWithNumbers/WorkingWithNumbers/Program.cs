4ee291a31cb232b9f6b7e582c4e90ce7f8d7d16a
Lance <Metal_Death_Music@yahoo.com>
2022-03-18 16:03:04 -0400

    Working With Numbers

---
+++ WorkingWithNumbers/WorkingWithNumbers/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace WorkingWithNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num = 6;
+
+            num++;
+
+            Console.WriteLine(-5.783);
+            Console.WriteLine(5 + 8);
+            Console.WriteLine(5 - 8);
+            Console.WriteLine(5 * 8);
+            Console.WriteLine(5 / 8);
+            Console.WriteLine(5 % 2);
+            Console.WriteLine(4 + 2 * 3);
+            Console.WriteLine((4 + 2) * 3);
+            Console.WriteLine(5.0 + 8.1);
+            Console.WriteLine(5 + 8.1);
+            Console.WriteLine(5 / 2);
+            Console.WriteLine(5 / 2.0);
+            Console.WriteLine(num);
+
+
+            Console.WriteLine(Math.Abs(-40));
+            Console.WriteLine(Math.Pow(3, 2));
+            Console.WriteLine(Math.Sqrt(36));
+            Console.WriteLine(Math.Max(4, 90));
+            Console.WriteLine(Math.Min(4, 90));
+            Console.WriteLine(Math.Round(4.3));
+
+
+            Console.ReadLine();
+        }
+    }
+}
