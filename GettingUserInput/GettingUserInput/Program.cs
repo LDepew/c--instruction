5946346b1c368d0f5b803673be4d259a60a9dead
Lance <Metal_Death_Music@yahoo.com>
2022-03-21 14:11:11 -0400

    Getting User Input

---
+++ GettingUserInput/GettingUserInput/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GettingUserInput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter your age: ");
+            string age = Console.ReadLine();
+            Console.WriteLine("Hello " + name + " you are " + age);
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
