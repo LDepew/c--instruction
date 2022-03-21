dfc21df58c10ca5b2382a8cd730746c54a18ed67
Lance <Metal_Death_Music@yahoo.com>
2022-03-21 14:59:33 -0400

    Building A Mad Libs Game

---
+++ BuildingAMadLibsGame/BuildingAMadLibsGame/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace BuildingAMadLibsGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string color, pluralNoun, celebrity;
+
+            Console.Write("Enter a color: ");
+            color = Console.ReadLine();
+
+            Console.WriteLine("Enter a plural noun: ");
+            pluralNoun = Console.ReadLine();
+
+            Console.Write("Enter a celebrity: ");
+            celebrity = Console.ReadLine();
+
+            Console.WriteLine();
+
+            Console.WriteLine("Roses are " + color);
+            Console.WriteLine(pluralNoun + " are blue");
+            Console.WriteLine("I Love " + celebrity);
+
+            Console.ReadLine();
+        }
+    }
+}
