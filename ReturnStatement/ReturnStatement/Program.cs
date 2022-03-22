ed958e0115624d2bf708f7a0129a2d962f211884
Lance <Metal_Death_Music@yahoo.com>
2022-03-22 14:52:59 -0400

    Return Statements

---
+++ ReturnStatement/ReturnStatement/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ReturnStatement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cubedNumber = cube(5);
+            Console.WriteLine(cubedNumber);
+
+            Console.WriteLine(cube(5));
+
+            Console.ReadLine();   
+        }
+
+        static int cube(int num)
+        {
+            int result = num * num * num;
+            return result;
+        }
+    }
+}
