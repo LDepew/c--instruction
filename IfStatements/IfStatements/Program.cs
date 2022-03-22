ec19510d87ba11e7944a1e50e04ba7b3d420b91a
Lance <Metal_Death_Music@yahoo.com>
2022-03-22 15:23:52 -0400

    If Statements

---
+++ IfStatements/IfStatements/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace IfStatements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isMale = false;
+
+            if (isMale)
+            {
+                Console.WriteLine("You are male");
+            } else
+            {
+                Console.WriteLine("You are not male");
+            }
+                 
+
+            Console.ReadLine();
+        }
+    }
+}
