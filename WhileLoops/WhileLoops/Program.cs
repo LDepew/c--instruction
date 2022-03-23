c8c4793fea8b59fe4a142cb9d6a7ee7ed18e37dc
Lance <Metal_Death_Music@yahoo.com>
2022-03-23 14:58:58 -0400

    While Loops

---
+++ WhileLoops/WhileLoops/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace WhileLoops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int index = 1;
+
+            while(index <= 5)
+            {
+                Console.WriteLine(index);
+                index++;
+            }
+
+            /* do
+             * {
+             *   Console.WriteLine(index);
+             *   index++;
+             * } while(index <= 5)
+             * */
+
+            Console.ReadLine();
+        }
+    }
+}
