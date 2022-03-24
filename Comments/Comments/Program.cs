cdf85b9aba51327f86ca9ddf5c139968c2417e5e
Lance <Metal_Death_Music@yahoo.com>
2022-03-24 17:14:20 -0400

    Comments

---
+++ Comments/Comments/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Comments
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Comments are fun");
+
+            //this is a single line comment
+
+            /* this
+             * is a
+             * multi-line
+             * comment
+             */
+
+            Console.ReadLine();
+        }
+    }
+}
