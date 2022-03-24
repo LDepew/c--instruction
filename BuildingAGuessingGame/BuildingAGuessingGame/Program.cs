4061da8ee26841cf0b34e993fba80968c070f24d
Lance <Metal_Death_Music@yahoo.com>
2022-03-24 14:55:41 -0400

    Building A Guessing Game

---
+++ BuildingAGuessingGame/BuildingAGuessingGame/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BuildingAGuessingGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string secretWord = "giraffe";
+            string guess = "";
+
+            while (guess != secretWord)
+            {
+                Console.Write("Enter guess: ");
+                guess = Console.ReadLine();
+            }
+
+            Console.Write("You win!");
+
+            Console.ReadLine();
+        }
+    }
+}
