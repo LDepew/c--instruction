45645702a6b8e2e1b0d9d728f94ea96b88af8726
Lance <Metal_Death_Music@yahoo.com>
2022-03-25 15:50:56 -0400

    Constructors

--- Constructors/Constructors/Program.cs
+++ Constructors/Constructors/Program.cs
@@ -6,15 +6,16 @@ namespace Constructors
     {
         static void Main(string[] args)
         {
-            Book book1 = new Book();
-            book1.title = "Harry Potter";
-            book1.author = "JK Rowling";
-            book1.pages = 400;
+            Book book1 = new Book("Harry Potter", "JK Rowling", 400);
+            
 
-            Book book2 = new Book();
-            book2.title = "Lord of the Rings";
-            book2.author = "Tolkien";
-            book2.pages = 700;
+            Book book2 = new Book("Lord of the Rings", "Tolkien", 700);
+
+            Book book3 = new Book();
+
+            book2.title = "The Hobbit";
+
+            Console.WriteLine(book2.title);
 
             Console.ReadLine();
         }
