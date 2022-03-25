db1fcb1014ad3cde3df9b9e545c9f6051ee51d36
Lance <Metal_Death_Music@yahoo.com>
2022-03-25 15:24:35 -0400

    Classes And Objects

---
+++ ClassesAndObjects/ClassesAndObjects/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ClassesAndObjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book();
+            book1.title = "Harry Potter";
+            book1.author = "JK Rowling";
+            book1.pages = 400;
+
+            Book book2 = new Book();
+            book2.title = "Lord of the Rings";
+            book2.author = "Tolkien";
+            book2.pages = 700;
+
+            Console.WriteLine(book1.pages);
+
+            Console.WriteLine(book2.author);
+
+            Console.ReadLine();
+        }
+    }
+}
