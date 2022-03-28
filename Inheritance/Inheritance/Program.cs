154260a90759d0c7d255fc801ce15ce86a79ab35
Lance <Metal_Death_Music@yahoo.com>
2022-03-28 17:19:46 -0400

    Inheritance

---
+++ Inheritance/Inheritance/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Chef chef = new Chef();
+            chef.MakeChicken();
+
+            ItalianChef italianChef = new ItalianChef();
+            italianChef.MakeChicken();
+            italianChef.MakePasta();
+            italianChef.MakeSpecialDish();
+
+            Console.ReadLine();
+        }
+    }
+}
