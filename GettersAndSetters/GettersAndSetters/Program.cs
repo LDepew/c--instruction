265ec497a00d2f5e32ff1efd4284a7410cc53ccb
Lance <Metal_Death_Music@yahoo.com>
2022-03-28 14:50:37 -0400

    Getters And Setters

---
+++ GettersAndSetters/GettersAndSetters/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GettersAndSetters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Movie avengers = new Movie("The Avengers", "Joss Whedon", "PG-13");
+            Movie shrek = new Movie("ShreK", "Adam Adamson", "PG");
+            // G, PG, PG-13, R, NR
+
+            Console.WriteLine(avengers.rating);
+
+            Console.ReadLine();
+        }
+    }
+}
