5a22c733d5c4e1771bc2774a8a9bd70de734b523
Lance <Metal_Death_Music@yahoo.com>
2022-03-28 16:57:58 -0400

    Static Methods And Classes

---
+++ StaticMethodsAndClasses/StaticMethodsAndClasses/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace StaticMethodsAndClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Math.Sqrt(144));
+
+            UsefulTools.sayHi("Lance");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StaticMethodsAndClasses/StaticMethodsAndClasses/UsefulTools.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticMethodsAndClasses
+{
+    static class UsefulTools
+    {
+
+        public static void sayHi(string name)
+        {
+            Console.WriteLine("Hello " + name);
+        }
+    }
+}
