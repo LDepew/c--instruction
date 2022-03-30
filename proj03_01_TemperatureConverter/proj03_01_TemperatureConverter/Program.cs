e8805ad9c7e38b09ddb7e3e325c1903cc1271d4b
Lance <Metal_Death_Music@yahoo.com>
2022-03-30 14:13:32 -0400

    Temperature Converter

---
+++ proj03_01_TemperatureConverter/proj03_01_TemperatureConverter/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace proj03_01_TemperatureConverter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Temperature Converter");
+            Console.WriteLine();
+
+            Console.WriteLine("Enter degrees in Fahrenheit: ");
+            double fahrenheit = Convert.ToDouble(Console.ReadLine());
+
+            double celsius;
+            celsius = (fahrenheit - 32) * 5 / 9;
+
+            Console.WriteLine("Degrees in Celsius: " + (Math.Round(celsius, 2)));
+
+            Console.ReadLine();
+        }
+    }
+}
