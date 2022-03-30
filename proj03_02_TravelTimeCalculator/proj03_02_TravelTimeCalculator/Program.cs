b5d69d4f0e2e6b1f7071ec879f04423f60c0b9c2
Lance <Metal_Death_Music@yahoo.com>
2022-03-30 14:52:05 -0400

    Travel Time Calculator

---
+++ proj03_02_TravelTimeCalculator/proj03_02_TravelTimeCalculator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace proj03_02_TravelTimeCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Travel Time Calculator");
+            Console.WriteLine();
+
+            Console.Write("Enter miles: ");
+            double miles = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter miles per hour: ");
+            double mph = Convert.ToDouble(Console.ReadLine());
+
+            double hours = miles / mph;
+
+            double minutes = miles % mph;
+
+            Console.WriteLine("Estimated travel time");
+            Console.WriteLine("---------------------");
+            Console.WriteLine("Hours: " + (Math.Round(hours, 0)));
+            Console.WriteLine("Minutes: " + minutes);
+
+            Console.ReadLine();
+        }
+    }
+}
