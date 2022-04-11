2af77185c4b308285eb44658f20368db1884fe0e
Lance <Metal_Death_Music@yahoo.com>
2022-04-11 16:53:48 -0400

    Table of Powers basic functionality

---
+++ proj04_01_TableOfPowers/proj04_01_TableOfPowers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace proj04_01_TableOfPowers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Squares and Cubes Table");
+            Console.WriteLine();
+
+            Console.WriteLine("Enter an integer: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            int squared = (number * number);
+
+            int cubed = ((number * number) * number);
+
+            Console.WriteLine("Number  Squared  Cubed");
+            Console.WriteLine("======  =======  =====");
+
+            Console.WriteLine(number + "      " + squared + "    " + cubed);
+
+            Console.ReadLine();
+        }
+    }
+}
