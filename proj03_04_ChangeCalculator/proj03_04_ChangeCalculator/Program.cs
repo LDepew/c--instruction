05cc5595d36d0ab11febc8938aee905e5ab9677c
Lance <Metal_Death_Music@yahoo.com>
2022-04-11 16:22:05 -0400

    Change Calculator

---
+++ proj03_04_ChangeCalculator/proj03_04_ChangeCalculator/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace proj03_04_ChangeCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Change Calculator");
+            Console.WriteLine();
+
+            Console.Write("Enter number of cents (0-99): ");
+            int cents = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            int quarters = cents / 25;
+            Console.WriteLine("Quarters: " + quarters);
+
+            int quartremain = cents % 25;
+
+            int dimes = quartremain / 10;
+            Console.WriteLine("Dimes: " + dimes);
+
+            int dimesremain = quartremain % 10;
+
+            int nickels = dimesremain / 5;
+            Console.WriteLine("Nickels: " + nickels);
+
+            int nickelsremain = dimesremain % 5;
+
+            int pennies = nickelsremain / 1;
+            Console.WriteLine("Pennies: " + pennies);
+
+            Console.ReadLine();
+        }
+    }
+}
