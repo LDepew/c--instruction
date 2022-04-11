510d77977b51d7770f96f04c9c22bd7b94b964f0
Lance <Metal_Death_Music@yahoo.com>
2022-04-11 15:26:51 -0400

    Interest Calculator

---
+++ proj03_03_InterestCalculator/proj03_03_InterestCalculator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace proj03_03_InterestCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Interest Calculator");
+
+            Console.Write("Enter loan amount:");
+            decimal loanamt = Convert.ToDecimal(Console.ReadLine());
+            string loanamount = String.Format("{0:C}", loanamt);
+
+            Console.Write("Enter interest rate:");
+            decimal intrate = Convert.ToDecimal(Console.ReadLine());
+
+            decimal interest = loanamt * intrate;
+            string interestamt = String.Format("{0:C}", interest);
+
+            Console.WriteLine("Loan amount: " + loanamount);
+
+            Console.WriteLine("Interest rate: " + intrate.ToString("0.000") + "%");
+
+            Console.WriteLine("Interest: " + interestamt);
+
+            Console.ReadLine();
+        }
+    }
+}
