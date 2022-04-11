bb5c75d885472a8c35dad427b5b6efbfd3069a20
Lance <Metal_Death_Music@yahoo.com>
2022-04-11 15:37:07 -0400

    Add run again function to Interest Calculator

--- proj03_03_InterestCalculator/proj03_03_InterestCalculator/Program.cs
+++ proj03_03_InterestCalculator/proj03_03_InterestCalculator/Program.cs
@@ -6,25 +6,46 @@ namespace proj03_03_InterestCalculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the Interest Calculator");
 
-            Console.Write("Enter loan amount:");
-            decimal loanamt = Convert.ToDecimal(Console.ReadLine());
-            string loanamount = String.Format("{0:C}", loanamt);
+            bool runagain = true;
 
-            Console.Write("Enter interest rate:");
-            decimal intrate = Convert.ToDecimal(Console.ReadLine());
+            while (runagain)
+            {
+                Console.WriteLine("Welcome to the Interest Calculator");
 
-            decimal interest = loanamt * intrate;
-            string interestamt = String.Format("{0:C}", interest);
+                Console.Write("Enter loan amount: ");
+                decimal loanamt = Convert.ToDecimal(Console.ReadLine());
+                string loanamount = String.Format("{0:C}", loanamt);
 
-            Console.WriteLine("Loan amount: " + loanamount);
+                Console.Write("Enter interest rate: ");
+                decimal intrate = Convert.ToDecimal(Console.ReadLine());
 
-            Console.WriteLine("Interest rate: " + intrate.ToString("0.000") + "%");
+                decimal interest = loanamt * intrate;
+                string interestamt = String.Format("{0:C}", interest);
 
-            Console.WriteLine("Interest: " + interestamt);
+                Console.WriteLine("Loan amount: " + loanamount);
 
-            Console.ReadLine();
+                Console.WriteLine("Interest rate: " + intrate.ToString("0.000") + "%");
+
+                Console.WriteLine("Interest: " + interestamt);
+
+                Console.Write("Continue (y/n): ");
+
+                string loop = Console.ReadLine();
+
+                if (loop == "y")
+                {
+                    runagain = true;
+                    Console.Clear();
+                }
+                else if (loop == "n")
+                {
+                    runagain = false;
+                }
+
+                Console.ReadLine();
+
+            }
         }
     }
 }
