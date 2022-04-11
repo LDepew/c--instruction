dbd57f270d94b7ad6614793cd942af86f3bf5031
Lance <Metal_Death_Music@yahoo.com>
2022-04-11 16:36:26 -0400

    Add run again function to Change Calculator

--- proj03_04_ChangeCalculator/proj03_04_ChangeCalculator/Program.cs
+++ proj03_04_ChangeCalculator/proj03_04_ChangeCalculator/Program.cs
@@ -9,27 +9,45 @@ namespace proj03_04_ChangeCalculator
             Console.WriteLine("Welcome to the Change Calculator");
             Console.WriteLine();
 
-            Console.Write("Enter number of cents (0-99): ");
-            int cents = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine();
+            bool runagain = true;
 
-            int quarters = cents / 25;
-            Console.WriteLine("Quarters: " + quarters);
+            while (runagain)
+            {
 
-            int quartremain = cents % 25;
+                Console.Write("Enter number of cents (0-99): ");
+                int cents = Convert.ToInt32(Console.ReadLine());
 
-            int dimes = quartremain / 10;
-            Console.WriteLine("Dimes: " + dimes);
+                Console.WriteLine();
 
-            int dimesremain = quartremain % 10;
+                int quarters = cents / 25;
+                Console.WriteLine("Quarters: " + quarters);
 
-            int nickels = dimesremain / 5;
-            Console.WriteLine("Nickels: " + nickels);
+                int quartremain = cents % 25;
 
-            int nickelsremain = dimesremain % 5;
+                int dimes = quartremain / 10;
+                Console.WriteLine("Dimes: " + dimes);
 
-            int pennies = nickelsremain / 1;
-            Console.WriteLine("Pennies: " + pennies);
+                int dimesremain = quartremain % 10;
+
+                int nickels = dimesremain / 5;
+                Console.WriteLine("Nickels: " + nickels);
+
+                int nickelsremain = dimesremain % 5;
+
+                int pennies = nickelsremain / 1;
+                Console.WriteLine("Pennies: " + pennies);
+
+                Console.Write("Continue (y/n): ");
+                string loop = Console.ReadLine();
+
+                if (loop == "y")
+                {
+                    runagain = true;
+                }
+                else if (loop == "y") {
+                    runagain = false;
+                }
+            }
 
             Console.ReadLine();
         }
