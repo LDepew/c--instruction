08fd77cce3a77b44e5bce8c0a2637adba58fe611
Lance <Metal_Death_Music@yahoo.com>
2022-04-11 15:50:10 -0400

    Add run again function to Temperature Calculator

--- proj03_01_TemperatureConverter/proj03_01_TemperatureConverter/Program.cs
+++ proj03_01_TemperatureConverter/proj03_01_TemperatureConverter/Program.cs
@@ -9,13 +9,32 @@ namespace proj03_01_TemperatureConverter
             Console.WriteLine("Welcome to the Temperature Converter");
             Console.WriteLine();
 
-            Console.WriteLine("Enter degrees in Fahrenheit: ");
-            double fahrenheit = Convert.ToDouble(Console.ReadLine());
+            bool runagain = true;
 
-            double celsius;
-            celsius = (fahrenheit - 32) * 5 / 9;
+            while (runagain)
+            {
 
-            Console.WriteLine("Degrees in Celsius: " + (Math.Round(celsius, 2)));
+                Console.WriteLine("Enter degrees in Fahrenheit: ");
+                double fahrenheit = Convert.ToDouble(Console.ReadLine());
+
+                double celsius;
+                celsius = (fahrenheit - 32) * 5 / 9;
+
+                Console.WriteLine("Degrees in Celsius: " + (Math.Round(celsius, 2)));
+
+                Console.Write("Continue (y/n): ");
+                string loop = Console.ReadLine();
+
+                if (loop == "y")
+                {
+                    runagain = true;
+                }
+                else if (loop == "n")
+                {
+                    runagain = false;
+                }
+
+            }
 
             Console.ReadLine();
         }
