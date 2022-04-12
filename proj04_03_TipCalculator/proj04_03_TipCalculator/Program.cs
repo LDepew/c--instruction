68a573027e5e2d88a2067850c99331accc68ce66
Lance <Metal_Death_Music@yahoo.com>
2022-04-12 17:08:51 -0400

    enhanced Tip Calculator

--- proj04_03_TipCalculator/proj04_03_TipCalculator/Program.cs
+++ proj04_03_TipCalculator/proj04_03_TipCalculator/Program.cs
@@ -11,18 +11,37 @@ namespace proj04_03_TipCalculator
 
             Console.Write("Cost of meal: ");
             decimal cost = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine();
 
             decimal tipfifteen = cost * 0.15m;
+            string tipfifteenamt = string.Format("{0:C}", tipfifteen);
+            decimal totalcostfifteen = cost + tipfifteen;
+            string totalcostfifteenamt = string.Format("{0:C}", totalcostfifteen);
 
             decimal tiptwenty = cost * 0.20m;
+            string tiptwentyamt = string.Format("{0:C}", tiptwenty);
+            decimal totalcosttwenty = cost + tiptwenty;
+            string totalcosttwentyamt = string.Format("{0:C}", totalcosttwenty);
 
             decimal tiptwentyfive = cost * 0.25m;
+            string tiptwentyfiveamt = string.Format("{0:C}", tiptwentyfive);
+            decimal totalcosttwentyfive = cost + tiptwentyfive;
+            string totalcosttwentyfiveamt = string.Format("{0:C}", totalcosttwentyfive);
 
-            Console.WriteLine("Tip amount 15%: " + tipfifteen);
+            Console.WriteLine("15%");
+            Console.WriteLine("Tip amount 15%: " + tipfifteenamt);
+            Console.WriteLine("Total amount: " + totalcostfifteenamt);
+            Console.WriteLine();
 
-            Console.WriteLine("Tip amount 20%: " + tiptwenty);
+            Console.WriteLine("20%");
+            Console.WriteLine("Tip amount 20%: " + tiptwentyamt);
+            Console.WriteLine("Total amount: " + totalcosttwentyamt);
+            Console.WriteLine();
 
-            Console.WriteLine("Tip amount 25%: " + tiptwentyfive);
+            Console.WriteLine("25%");
+            Console.WriteLine("Tip amount 25%: " + tiptwentyfiveamt);
+            Console.WriteLine("Total amount: " + totalcosttwentyfiveamt);
+            Console.WriteLine();
 
             Console.ReadLine();
         }
