0d1997c0c8f5eb88956cc9416868aededbf9439e
Lance <Metal_Death_Music@yahoo.com>
2022-04-12 16:33:56 -0400

    initial Tip Calculator

---
+++ proj04_03_TipCalculator/proj04_03_TipCalculator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace proj04_03_TipCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tip Calculator");
+            Console.WriteLine();
+
+            Console.Write("Cost of meal: ");
+            decimal cost = Convert.ToDecimal(Console.ReadLine());
+
+            decimal tipfifteen = cost * 0.15m;
+
+            decimal tiptwenty = cost * 0.20m;
+
+            decimal tiptwentyfive = cost * 0.25m;
+
+            Console.WriteLine("Tip amount 15%: " + tipfifteen);
+
+            Console.WriteLine("Tip amount 20%: " + tiptwenty);
+
+            Console.WriteLine("Tip amount 25%: " + tiptwentyfive);
+
+            Console.ReadLine();
+        }
+    }
+}
