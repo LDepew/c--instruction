4cc37537e8bdffe30cd640c55a6954624f92383e
Lance <Metal_Death_Music@yahoo.com>
2022-04-14 14:59:25 -0400

    Initial working program Data Validation

---
+++ proj05_02_DataValidation/proj05_02_DataValidation/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace proj05_02_DataValidation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Area and Perimeter Calculator");
+            Console.WriteLine();
+
+            bool runagain = true;
+
+            while (runagain)
+            {
+
+                Console.Write("Enter length: ");
+                double length = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Enter width: ");
+                double width = Convert.ToDouble(Console.ReadLine());
+
+                double area = width * length;
+                double perimeter = (2 * width) + (2 * length);
+
+                Console.WriteLine("Area: " + area);
+                Console.WriteLine("Perimeter: " + perimeter);
+
+                Console.WriteLine();
+                Console.Write("Continue? (y/n): ");
+                string loop = Console.ReadLine();
+                Console.WriteLine();
+
+                if (loop == "y")
+                {
+                    runagain = true;
+                }
+                else if (loop == "n")
+                {
+                    runagain = false;
+                }
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
