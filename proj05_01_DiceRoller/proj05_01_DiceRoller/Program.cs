dbbb684b5ad925d06996fb8e643a4ef5228cecef
Lance <Metal_Death_Music@yahoo.com>
2022-04-14 14:28:22 -0400

    Initial working Dice Game

---
+++ proj05_01_DiceRoller/proj05_01_DiceRoller/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace proj05_01_DiceRoller
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dice Roller");
+            Console.WriteLine();
+
+            Console.WriteLine("Roll the dice? (y/n)");
+
+            Random rand = new Random();
+
+            int die1 = (rand.Next(1, 7));
+
+            int die2 = (rand.Next(1, 7));
+
+            int total = die1 + die2;
+
+            Console.WriteLine("Die 1: " + die1);
+            Console.WriteLine("Die 2: " + die2);
+            Console.WriteLine("Total: " + total);
+
+            Console.ReadLine();
+        }
+    }
+}
