0f730b87cb359cf00a1ebffdbe2e2adfaa763ffb
Lance <Metal_Death_Music@yahoo.com>
2022-04-14 14:45:42 -0400

    enhanced working Dice Game

--- proj05_01_DiceRoller/proj05_01_DiceRoller/Program.cs
+++ proj05_01_DiceRoller/proj05_01_DiceRoller/Program.cs
@@ -9,20 +9,61 @@ namespace proj05_01_DiceRoller
             Console.WriteLine("Dice Roller");
             Console.WriteLine();
 
-            Console.WriteLine("Roll the dice? (y/n)");
+            bool runagain = true;
 
-            Random rand = new Random();
+        
+            Console.Write("Roll the dice? (y/n)");
+            string roll = Console.ReadLine();
 
-            int die1 = (rand.Next(1, 7));
+            if (roll == "y")
+            {
 
-            int die2 = (rand.Next(1, 7));
+                while (runagain)
+                {
+                    Random rand = new Random();
 
-            int total = die1 + die2;
+                    int die1 = (rand.Next(1, 7));
 
-            Console.WriteLine("Die 1: " + die1);
-            Console.WriteLine("Die 2: " + die2);
-            Console.WriteLine("Total: " + total);
+                    int die2 = (rand.Next(1, 7));
 
+                    int total = die1 + die2;
+
+                    Console.WriteLine("Die 1: " + die1);
+                    Console.WriteLine("Die 2: " + die2);
+                    Console.WriteLine("Total: " + total);
+
+                    if (die1 == 1 && die2 == 1)
+                    {
+                        Console.WriteLine("Snake eyes!");
+                    }
+                    else if (die1 == 6 && die2 == 6)
+                    {
+                        Console.WriteLine("Box cars!");
+                    }
+
+                    Console.WriteLine();
+                    Console.Write("Roll again? (y/n)");
+                    string loop = Console.ReadLine();
+
+                    if (loop == "y")
+                    {
+                        runagain = true;
+                    }
+                    else if (loop == "n")
+                    {
+                        runagain = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid Selection");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Goodbye");
+            }
+            
             Console.ReadLine();
         }
     }
