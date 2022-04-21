2a90e81c05515fb25d3b44efbcb90a33ca4087a3
Lance <Metal_Death_Music@yahoo.com>
2022-04-21 14:41:36 -0400

    ch1 ex2 TestScore conversion

---
+++ ch01_ex2_TestScore/ch01_ex2_TestScore/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ch01_ex2_TestScore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter test scores that range from 0 to 100.");
+            Console.WriteLine("To end the program, enter 999.");
+            Console.WriteLine();
+
+            int scoreTotal = 0;
+            int scoreCount = 0;
+            int testScore = 0;
+
+            while (testScore <= 100)
+            {
+                Console.Write("Enter score: ");
+                testScore = Convert.ToInt32(Console.ReadLine());
+
+                if (testScore <= 100)
+                {
+                    scoreCount = scoreCount + 1;
+                    scoreTotal = scoreTotal + testScore;
+                }
+            }
+
+            double averageScore = (double)scoreTotal / scoreCount;
+            String message = "\n"
+                    + "Score count:   " + scoreCount + "\n"
+                    + "Score total:   " + scoreTotal + "\n"
+                    + "Average score: " + averageScore + "\n";
+            Console.WriteLine(message);
+            Console.ReadLine();
+        }
+    }
+}
