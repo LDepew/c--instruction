d7fa1f62c2f74b7eb103a68e8eb19865d2a2c012
Lance <Metal_Death_Music@yahoo.com>
2022-04-21 14:33:49 -0400

    ch01 ex1 Invoice conversion

---
+++ ch01_ex1_Invoice/ch01_ex1_Invoice/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace ch01_ex1_Invoice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Invoice Total Calculator");
+            Console.WriteLine();
+
+
+            bool runagain = true;
+
+            while (runagain)
+            {
+                Console.WriteLine("Enter subtotal: ");
+                double subtotal = Convert.ToDouble(Console.ReadLine());
+
+                double discountPercent;
+                if (subtotal >= 200)
+                {
+                    discountPercent = .2;
+                }
+                else if (subtotal >= 100)
+                {
+                    discountPercent = .1;
+                }
+                else
+                {
+                    discountPercent = 0.0;
+                }
+                double discountAmount = subtotal * discountPercent;
+                double total = subtotal - discountAmount;
+
+                String message = "Discount percent: " + discountPercent + "\n"
+                               + "Discount amount:  " + discountAmount + "\n"
+                               + "Invoice total:    " + total + "\n";
+                Console.WriteLine(message);
+
+                Console.WriteLine("Continue? (y/n): ");
+                String loop = Console.ReadLine();
+
+                if (loop == "y")
+                {
+                    runagain = true;
+                }
+                else if (loop == "n")
+                {
+                    runagain = false;
+                }
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
