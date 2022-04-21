2dd2219d23f1ae411734cc1d18376ffad96c6e4a
Lance <Metal_Death_Music@yahoo.com>
2022-04-21 16:11:07 -0400

    ch02 e3 Invoice enhancement

--- ch02_ex3_Invoice/ch02_ex3_Invoice/Program.cs
+++ ch02_ex3_Invoice/ch02_ex3_Invoice/Program.cs
@@ -11,13 +11,20 @@ namespace ch02_ex3_Invoice
 
             bool runagain = true;
 
+            double totalAmount = 0;
+            double totalDiscount = 0;
+            int numberInvoices = 0;
+
             while (runagain) {
 
             Console.Write("Enter subtotal: ");
                 double subtotal = Convert.ToDouble(Console.ReadLine());
 
                 double discountPercent;
-                if (subtotal >= 200)
+                if (subtotal >= 500)
+                {
+                    discountPercent = .25;
+                } else if (subtotal >= 200)
                 {
                     discountPercent = .2;
                 } else if (subtotal >= 100)
@@ -30,6 +37,10 @@ namespace ch02_ex3_Invoice
                 double discountAmount = subtotal * discountPercent;
                 double total = subtotal - discountAmount;
 
+                totalAmount = totalAmount + total;
+                totalDiscount = totalDiscount + discountAmount;
+                numberInvoices = numberInvoices + 1;
+
                 String message = "Discount percent:  " + discountPercent + "\n"
                                + "Discount amount:   " + discountAmount + "\n"
                                + "Invoice total:     " + total + "\n";
@@ -38,16 +49,25 @@ namespace ch02_ex3_Invoice
                 Console.Write("Continue? (y/n): ");
                 string loop = Console.ReadLine();
 
-                if (loop == "y")
+                if (loop == "y" || loop == "Y")
                 {
                     runagain = true;
                 }
-                else if (loop == "n")
+                else if (loop == "n" || loop == "N")
                 {
                     runagain = false;
                 }
+                else
+                {
+                    runagain = true;
+                }
 
             }
+
+            string message2 = "Number of invoices:  " + numberInvoices + "\n"
+                            + "Average invoice:     " + totalAmount/ numberInvoices + "\n"
+                            + "Average discount:    " + totalDiscount / numberInvoices + "\n";
+            Console.WriteLine(message2);
 
             Console.ReadLine();
         }
