d65db34cd8a3f2e28f912dedcfec3f318d9db54a
Lance <Metal_Death_Music@yahoo.com>
2022-04-21 16:20:40 -0400

    ch03 ex1 MPG conversion

---
+++ ch03_ex1_MPG/ch03_ex1_MPG/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ch03_ex1_MPG
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Miles Per Gallon calculator");
+            Console.WriteLine();
+
+            bool runagain = true;
+
+            while (runagain)
+            {
+
+                Console.Write("Enter miles driven: ");
+                double miles = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Enter gallons of gas used: ");
+                double gallons = Convert.ToDouble(Console.ReadLine());
+
+                double mpg = miles / gallons;
+                Console.WriteLine("Miles per gallon is " + mpg + ".");
+                Console.WriteLine();
+
+                Console.Write("Calculate another MPG? (y/n): ");
+                string loop = Console.ReadLine();
+
+                if (loop == "y")
+                {
+                    runagain = true;
+                }
+                else if (loop == "n")
+                {
+                    runagain = false;
+                }
+
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
