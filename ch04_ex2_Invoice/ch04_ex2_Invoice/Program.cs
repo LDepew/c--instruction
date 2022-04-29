b669ebe3566f8eea70008f5852970aa1e23d34b9
Lance <Metal_Death_Music@yahoo.com>
2022-04-29 15:42:58 -0400

    ch04 ex2 Invoice enhancement

--- ch04_ex2_Invoice/ch04_ex2_Invoice/Program.cs
+++ ch04_ex2_Invoice/ch04_ex2_Invoice/Program.cs
@@ -10,13 +10,13 @@ namespace ch04_ex2_Invoice
 
             while (runagain)
             {
-                Console.Write("Enter customer type (r/c): ");
+                Console.Write("Enter customer type (r/c/t): ");
                 String customerType = Console.ReadLine();
                 Console.Write("Enter subtotal: ");
                 double subtotal = Convert.ToDouble(Console.ReadLine());
 
                 double discountPercent = 0.0;
-                switch (customerType)
+                /*switch (customerType)
                 {
                     case "r":
                     case "R":
@@ -28,26 +28,70 @@ namespace ch04_ex2_Invoice
                         {
                             discountPercent = .1;
                         }
-                        else if (subtotal >= 250)
+                        else if (subtotal >= 250 && subtotal < 500)
                         {
-                            discountPercent = .2;
-                        }
-                        break;
-                    case "c":
-                    case "C":
-                        if (subtotal < 250)
-                        {
-                            discountPercent = .2;
-                        }
-                        else if (subtotal >= 250)
+                            discountPercent = .25;
+                        } 
+                        else if (subtotal > 500)
                         {
                             discountPercent = .3;
                         }
                         break;
-                    default:
-                        discountPercent = .1;
+                    case "c":
+                    case "C":
+                        discountPercent = .2;
                         break;
+                    case "t":
+                    case "T":
+                        if (subtotal < 500)
+                        {
+                            discountPercent = .4;
+                        }
+                        else if (subtotal >= 500)
+                        {
+                            discountPercent = .5;
+                        }
+                        break;
+                    default:
+                        discountPercent = 0.0;
+                        break;
+                }*/
+                if (customerType == "r" || customerType == "R"){
+
+                    if (subtotal < 100)
+                    {
+                        discountPercent = 0.0;
+                    }
+                    else if (subtotal >= 100 && subtotal < 250)
+                    {
+                        discountPercent = .1;
+                    }
+                    else if (subtotal >= 250 && subtotal < 500)
+                    {
+                        discountPercent = .25;
+                    }
+                    else if (subtotal > 500)
+                    {
+                        discountPercent = .3;
+                    }
                 }
+                else if (customerType == "c" || customerType == "C") {
+                    discountPercent = .2;
+                }
+                else if (customerType == "t" || customerType == "T") {
+                    if (subtotal < 500)
+                    {
+                        discountPercent = .4;
+                    }
+                    else if (subtotal >= 500)
+                    {
+                        discountPercent = .5;
+                    }
+                }
+                else {
+                    discountPercent = 0.0;
+                }
+                                
 
                 double discountAmount = subtotal * discountPercent;
                 discountAmount = Math.Ceiling(discountAmount * 100) / 100;
