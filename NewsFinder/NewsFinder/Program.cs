27bbba4a2995f2ba2e605a3d9afdf28b2fafd8a4
Lance <Metal_Death_Music@yahoo.com>
2022-11-01 16:07:21 -0400

    News Finder v.3

--- NewsFinder/NewsFinder/Program.cs
+++ NewsFinder/NewsFinder/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using static System.Net.WebRequestMethods;
 
 namespace NewsFinder
 {
@@ -7,18 +8,37 @@ namespace NewsFinder
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter search terms: ");
-            String query = Console.ReadLine();
+            Console.WriteLine("Youtube or Google: ");
+            String selection = Console.ReadLine();
 
-            Console.WriteLine(query);
-
-            string request = "https://www.google.com/search?q=" + query + "&sxsrf=ALiCzsaYhvj3AIkdOBeYwbV-p8H1ZYIYPA%3A1667163884606&source=hp&ei=7OZeY-iYIuas5NoPwt-LeA&iflsig=AJiK0e8AAAAAY170_HkY-d2394byCTrEswIk2hcwc37c&ved=0ahUKEwjovvy47Yj7AhVmFlkFHcLvAg8Q4dUDCAs&uact=5&oq=" + query + "&gs_lcp=Cgdnd3Mtd2l6EAMyBQgAEJECMgcIABCxAxBDMgcILhCxAxBDMgQIABBDMgoIABCxAxDJAxBDMgQILhBDMggIABCABBCxAzIECAAQQzILCC4QgAQQsQMQgwEyBAgAEEM6BAgjECc6BQguEIAEOgUIABCABDoICC4QsQMQgwE6EQguEIAEELEDEIMBEMcBENEDOg4ILhCABBCxAxCDARDUAjoQCC4QsQMQgwEQxwEQ0QMQQzoGCAAQChBDOgcIABDJAxBDUABY1wRg2wVoAHAAeACAAWOIAaMEkgEBNpgBAKABAQ&sclient=gws-wiz";
-            ProcessStartInfo ps = new ProcessStartInfo
+            if (selection == "Google" || selection == "google")
             {
-                FileName = request,
-                UseShellExecute = true
-            };
-            Process.Start(ps);
+                Console.WriteLine("Enter search terms: ");
+                String query = Console.ReadLine();
+
+                Console.WriteLine(query);
+
+                string request = "https://www.google.com/search?q=" + query + "&sxsrf=ALiCzsaYhvj3AIkdOBeYwbV-p8H1ZYIYPA%3A1667163884606&source=hp&ei=7OZeY-iYIuas5NoPwt-LeA&iflsig=AJiK0e8AAAAAY170_HkY-d2394byCTrEswIk2hcwc37c&ved=0ahUKEwjovvy47Yj7AhVmFlkFHcLvAg8Q4dUDCAs&uact=5&oq=" + query + "&gs_lcp=Cgdnd3Mtd2l6EAMyBQgAEJECMgcIABCxAxBDMgcILhCxAxBDMgQIABBDMgoIABCxAxDJAxBDMgQILhBDMggIABCABBCxAzIECAAQQzILCC4QgAQQsQMQgwEyBAgAEEM6BAgjECc6BQguEIAEOgUIABCABDoICC4QsQMQgwE6EQguEIAEELEDEIMBEMcBENEDOg4ILhCABBCxAxCDARDUAjoQCC4QsQMQgwEQxwEQ0QMQQzoGCAAQChBDOgcIABDJAxBDUABY1wRg2wVoAHAAeACAAWOIAaMEkgEBNpgBAKABAQ&sclient=gws-wiz";
+                ProcessStartInfo ps = new ProcessStartInfo
+                {
+                    FileName = request,
+                    UseShellExecute = true
+                };
+                Process.Start(ps);
+            }
+            else if (selection == "Youtube" || selection == "youtube")
+            {
+                Console.WriteLine("Enter search terms: ");
+                String query = Console.ReadLine();
+
+                string request = "https://www.youtube.com/results?search_query=" + query;
+                ProcessStartInfo ps = new ProcessStartInfo
+                {
+                    FileName = request,
+                    UseShellExecute = true
+                };
+                Process.Start(ps);
+            }
         }
     }
 }
